ac0b1309667877ad7132aa1b832aa7e1cd92c2f7
CodeFreaker <1788436+codefreaker@users.noreply.github.com>
2019-03-28 06:38:29 +0530

    Initial file

---
+++ setup.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+from setuptools import setup
+setup(
+    name="randomintnumbers",    
+    version="1.0.0",
+    description="This package generates random integer numbers",
+    scripts=["randomintnumbers"]              
+    )
